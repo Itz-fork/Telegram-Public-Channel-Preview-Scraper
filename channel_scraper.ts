614b92bdbcf058a068209984a0d585fa36f7821e
Itz-fork <git.itzfork@gmail.com>
2022-08-24 17:48:26 +0530

    Accurate meta data

--- channel_scraper.ts
+++ channel_scraper.ts
@@ -22,12 +22,13 @@ async function remove_tags(html: any) {
 // deno-lint-ignore require-await
 async function get_images(msg: Element) {
   let imgs = [];
-  if (msg.previousElementSibling?.nodeName === "DIV") {
-    for (const cnt of msg.previousElementSibling.getElementsByTagName("a")) {
+  let pre = msg.previousElementSibling;
+  if (pre?.nodeName === "DIV") {
+    for (const cnt of pre.getElementsByTagName("a")) {
       imgs.push(cnt?.getAttribute("style")?.match(/(?<=\(')(.*?)(?='\))/)[0]);
     }
   } else {
-    imgs.push(msg?.getAttribute("style")?.match(/(?<=\(')(.*?)(?='\))/)[0]);
+    imgs.push(pre?.getAttribute("style")?.match(/(?<=\(')(.*?)(?='\))/)[0]);
   }
   return imgs;
 }
@@ -41,21 +42,21 @@ async function fetch_posts(channel: string) {
   const messages = html?.getElementsByClassName(
     "tgme_widget_message_text js-message_text",
   );
-  const footers = html?.getElementsByClassName(
-    "tgme_widget_message_footer compact js-message_footer",
-  );
 
   // Prepare messages
   let messages_list = [];
   for (const [index, msg] of messages.entries()) {
-    let foot = footers[index];
-    let vis = foot.getElementsByClassName("tgme_widget_message_views")
+    msg.nextElementSibling;
+    let foot = msg.nextElementSibling;
+    let vis = foot?.getElementsByClassName("tgme_widget_message_views")[0];
+    let purl = foot?.getElementsByClassName("tgme_widget_message_date")[0];
+    let tim = foot?.getElementsByTagName("time")[0];
+
     messages_list.push({
       text: await remove_tags(msg.innerHTML),
-      views: vis[0] ? vis[0].innerText : "",
-      post_url: foot.getElementsByClassName("tgme_widget_message_date")[0]
-        .getAttribute("href"),
-      time: foot.getElementsByTagName("time")[0].getAttribute("datetime"),
+      views: vis ? vis.innerText : "",
+      post_url: purl ? purl.getAttribute("href") : "",
+      time: tim ? tim.getAttribute("datetime") : "",
       media: await get_images(msg),
     });
   }
